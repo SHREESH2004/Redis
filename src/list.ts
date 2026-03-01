1060779a7726896645161de4eddc8f945a1ef9cd
Shreesh Sanyal <shreeshsanyal@gmail.com>
2026-03-02 00:00:34 +0530

    add ons

--- src/list.ts
+++ src/list.ts
@@ -2,6 +2,7 @@ import { client } from "./string.js";
 
 async function init(){
     await client.lpush('messages',1);
-    await client.l
+    await client.lpush('messages',1);
+    await client.blpop('messages',10);
 }
 init()
